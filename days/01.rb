a096495ca33bf74a4c3bf1f8d5a0595eeab63d1d
Chris Nevett <nevett@gmail.com>
2022-12-11 11:28:18 +0000

    Day 1

--- days/01.rb
+++ days/01.rb
@@ -1,9 +1,27 @@
 class Day01 < Day
   def part_1
-    "Example, here's the first line of input: #{input.lines.first}"
+    calorie_counts.max
   end
 
   def part_2
-    "Implement me in ./days/01.rb"
+    calorie_counts.sort.last(3).sum
+  end
+
+  def calorie_counts
+    acc = 0
+    calorie_counts = []
+    stripped_input_lines.each do |line|
+      line = line.strip
+      if line.empty?
+        calorie_counts << acc
+        acc = 0
+      else
+        acc += line.to_i
+      end
+    end
+
+    calorie_counts << acc
+
+    calorie_counts
   end
 end
